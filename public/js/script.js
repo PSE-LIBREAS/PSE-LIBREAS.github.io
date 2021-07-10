5fa5114c5c1c10c17db2df362487d1e22730890a
Sophia Dörner <38356908+doerners@users.noreply.github.com>
2021-07-10 16:16:47 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,12 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+function show(element) {
+var x = document.getElementById(element);
+if (x.style.display != "block") {
+x.style.display = "block";
+} else {
+x.style.display = "none";
+  }
+}
